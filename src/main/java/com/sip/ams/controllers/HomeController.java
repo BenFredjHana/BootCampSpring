6da448fce872be5b92c421405d6ea7a0451916ef
BenFredjHana <hana.bf2012@gmail.com>
2022-02-12 20:03:43 +0100

    ajouter formulaire

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,21 +1,43 @@
 package com.sip.ams.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
 public class HomeController {
-	
+
+	@PostMapping("/result")
+	@ResponseBody
+	public String resultat(@RequestParam("nom") String n, @RequestParam("email") String e) {
+		return ("Vous avez tapez : \n un nom: " + n + "\n un email: " + e);
+	}
+
 	@RequestMapping("/information")
-	public String info()
-	{
+	public String info(Model model) {
+		String formation = "FullStack";
+		model.addAttribute("workshop", formation);
+
+		ArrayList<String> tab = new ArrayList<>();
+
+		tab.add("OCA");
+		tab.add("OCP");
+		tab.add("Spring");
+		tab.add("Angular");
+
+		model.addAttribute("tab", tab);
+
 		return "Home/info";
 	}
 
 	@RequestMapping("/affichage")
-	public String affiche()
-	{
+	public String affiche() {
 		System.out.println("Méthode affichage");
 		return "Home/affichage";
 	}
